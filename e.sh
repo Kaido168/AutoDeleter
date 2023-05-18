9a5016517e7f69a51f932d033b97e495745097e7
Kaido168 <113695298+Kaido168@users.noreply.github.com>
2023-05-19 01:14:33 +0300

    Update e.sh

--- e.sh
+++ e.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
+day=$2
+directory_path=$1
+if [[ ! -n $1 ]]
+then
+echo "не указана директория"
+exit 1
+fi
 
-directory_path="путь к папке с файлами"
+if [[ ! -d $1 ]]
+then
+echo "это не директория"
+exit 2
+fi
+
+if [[ ! -n $2 ]]
+then
+echo "не указано количество суток, после которого надо удалять файлы"
+exit 3
+fi
+
+
 current_time=$(date +%s)
-for file in "directory_path"/*
+for file in "$directory_path"/*
 do
-    current_time=$(stat -c %Y "$file")
+    creation_time=$(stat -c %Y "$file")
     time_difference=$(( (current_time - creation_time) / (24 * 3600) ))
-    if [ "$time_difference" -gt 365 ]
+    if [ "$time_difference" -gt $day ]
     then
-        rm "file"
+        rm "$file"
     fi
 done
